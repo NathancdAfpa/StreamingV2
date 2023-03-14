75eebe2f39dbf5d4efdbe6caa2fb6951faca24e8
Vick-17 <vega.victor.0712@gmail.com>
2023-03-14 14:48:04 +0100

    third commit

--- src/main/java/com/streaming2/Controller.java
+++ src/main/java/com/streaming2/Controller.java
@@ -1,12 +1,15 @@
 package com.streaming2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Controller {
 
@@ -31,4 +34,44 @@ public class Controller {
     @FXML
     private TextField tfMdp;
 
+    @FXML
+    private TableView<?> TableVList;
+
+    @FXML
+    private TableView<?> TableVPlaylist;
+
+    @FXML
+    private Button btnAjouter;
+
+    @FXML
+    private Label btnCreate;
+
+    @FXML
+    private Button btnRetour;
+
+    @FXML
+    private Button btnSupprimer;
+
+    @FXML
+    private Label labelList;
+
+    @FXML
+    private Label labelNom;
+
+    @FXML
+    private Label labelPlaylist;
+
+    @FXML
+    private Label labelPrenom;
+
+    //information de conexion
+/*    String mail = tfMail.getText();
+    String mdp = tfMdp.getText();*/
+
+    public void affichageInfoUser(ActionEvent event) {
+/*        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Streaming");
+        stage.setScene(scene);*/
+    }
 }
